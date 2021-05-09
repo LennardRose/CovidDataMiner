127e640940ce278f03bbace39698bfcfbe3d135a
Moritz Zeitler <moritz.zeitler@web.de>
2021-05-09 21:32:48 +0200

    initial commit, districts test

---
+++ RKiLoader/Config.py
@@ -0,0 +1,9 @@
+#-------------------------------miscellaneous------------------------------------------
+path_to_city_csv = './Cities.csv'
+
+german_state_list = ['Hamburg', 'Bremen', 'Bayern','Baden-Wuertemberg', 'Sachsen', 'Sachsen-Anhalt', 'Thueringen', 'Hessen', 'Schleswig-Holstein','Brandenburg', 'Nordrhein-Westfalen','Saarland','Niedersachsen', 'Berlin', 'Mecklenburg-Vorpommern', 'Rheinland-Pfalz']
+
+corona_api_base_url = 'https://api.corona-zahlen.org/'
+
+corona_api_states = 'states/'
+corona_api_districts = 'districts/'
---
+++ RKiLoader/main.py
@@ -0,0 +1,11 @@
+# This is a sample Python script.
+import requests
+from Config import *
+import pandas as pd
+
+
+if __name__ == "__main__":
+    city_df = pd.read_csv(path_to_city_csv, header=0, sep=';')
+    for index, row in city_df.iterrows():
+        #print(corona_api_base_url + corona_api_districts + row['AGS'][0:5])
+        print(requests.get(corona_api_base_url + corona_api_districts + row['AGS'][0:5]).json())
