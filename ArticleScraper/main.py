bcc36faab8f7338d50b6fe58ab4dfd7b2a77177a
LennardRose <lennard.rose@student.fhws.de>
2021-05-27 21:32:45 +0200

    poc scraper läuft

--- ArticleScraper/main.py
+++ ArticleScraper/main.py
@@ -1,25 +1,33 @@
 import requests
-import re
 from bs4 import BeautifulSoup
-import urllib
-from datetime import datetime
+from lxml import html
 import ssl
 import os
 
 
-ssl._create_default_https_context = ssl._create_unverified_context
-URL = "https://www.sueddeutsche.de/thema/Coronavirus_in_M%C3%BCnchen"
-
-if __name__ == '__main__':
-    #seite holen
+def get_soup_out_of_page(URL):
+    
     page = requests.get(URL)
+    return BeautifulSoup(page.content, 'html.parser')
 
-    #seite aufbereiten
-    soup = BeautifulSoup(page.content, 'html.parser')
-    encoding = soup.original_encoding or 'utf-8'
+def get_articlelink_list(URL):
+
+    soup = get_soup_out_of_page(URL)
+    articlelist = soup.body.find_all('a', class_="sz-teaser" )#<--CSS selector um auf die liste zuzugreifen 
+    links = []
+    for row in articlelist: #brauch ne mapfunktion, beautifulsoup hat keine doku 
+        if row:
+            links.append(row['href'])
+    return links
+    #erster link ist immer der aktuellste, vielleicht speichern und dann abgleichen, dass man nur immer die läd die man noch nicht hat        
+
+def get_content_of_page(URL):
+
+    soup = get_soup_out_of_page(URL)
+    encoding = soup.original_encoding or 'utf-8' #encoding für sonderzeichen sonst heult er rum
     text = str(soup.encode(encoding)).split("\\n") #er checkt einfach nicht dass das hier lineseperators sind
 
-    #metadaten auslesen um namen für file zu bekommen
+    #metadaten auslesen um namen für file zu bekommen, besser dann das datum des tages an dem gescraped wurde einfügen
     for tag in soup.find_all("meta"):
         if tag.get("name", None) == "last-modified":
             date = str(tag.get("content", None)).replace(" ", "_").replace(",", "_").replace(":", "_")
@@ -32,5 +40,10 @@ if __name__ == '__main__':
             file.write(line + os.linesep)
 
 
-            #next steps: das ist der content der übersichtsseite, die artikel sind unter /html/body/div[3]/div/div/div[1]/div[4]/div[1]/div[1]/a bzw. html.__ninja_cookie_safari body.muenchen.global div#sueddeutsche.site div#wrapperbelt.site__wrapper div#wrapper.site__wrapper__content div.sz-page.sz-clearfix div.mainpage.theme.sz-page__main-column div.teaserlist div.sz-teaserlist-element a.sz-teaser
-            #den dann auslesen bzw. die href links speichern und dann alle verlinkten artikel auslesen
+if __name__ == '__main__':
+
+    ssl._create_default_https_context = ssl._create_unverified_context
+    URL = "https://www.sueddeutsche.de/thema/Coronavirus_in_M%C3%BCnchen"   
+
+    print(get_articlelink_list(URL))
+    
